2f043644d856224ed6f8a602b972301f3652ebb5
ali-jee-786 <786syedalihussain@gmail.com>
2024-08-06 08:19:42 +0500

    updated

--- blog/urls.py
+++ blog/urls.py
@@ -1,2 +1,6 @@
 from django.urls import path
+from .views import BlogAPI
 
+urlpatterns = [
+    path('', BlogAPI.as_view())
+]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from .serializers import BlogSerializer
-
-
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class BlogAPI(APIView):
     permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    
     def post(self, request):
         data = request.data
         serializer = BlogSerializer(data=data)
+        if not serializer.is_valid():
+            return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        
+
